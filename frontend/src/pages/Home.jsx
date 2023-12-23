b001a9dab781fef7e12da81350fdf0c5c151eb5c
YasithaRDK <yasithadilshanrajapaksha@gmail.com>
2023-12-24 02:30:17 +0530

    set spinner for home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,10 +4,16 @@ import {
   useGetStudentsQuery,
   useDeleteStudentMutation,
 } from "../features/studentApi";
+import Loader from "../components/Loader";
 
 const Home = () => {
-  const { data: students, refetch } = useGetStudentsQuery();
-  const [deleteStudent] = useDeleteStudentMutation();
+  const {
+    data: students,
+    refetch,
+    isLoading: getStudentsLoading,
+  } = useGetStudentsQuery();
+  const [deleteStudent, { isLoading: deleteStudentLoading }] =
+    useDeleteStudentMutation();
 
   const onDeleteStudent = async (id) => {
     await deleteStudent(id);
@@ -18,47 +24,51 @@ const Home = () => {
     <Container>
       <Card className="mt-5 p-2 p-md-3">
         <h1>Student Details</h1>
-        <Table responsive striped bordered hover>
-          <thead>
-            <tr>
-              <th>#</th>
-              <th>Name</th>
-              <th>Contact</th>
-              <th>Email</th>
-              <th>Action</th>
-            </tr>
-          </thead>
-          <tbody>
-            {students &&
-              students.map((student, index) => (
-                <tr key={index}>
-                  <td>{index + 1}</td>
-                  <td>{student.name}</td>
-                  <td>{student.contact}</td>
-                  <td>{student.email}</td>
-                  <td>
-                    <Link to={`edit/${student._id}`}>
-                      <Button variant="warning" className="me-2">
-                        Edit
+        {getStudentsLoading || deleteStudentLoading ? (
+          <Loader />
+        ) : (
+          <Table responsive striped bordered hover>
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Name</th>
+                <th>Contact</th>
+                <th>Email</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {students &&
+                students.map((student, index) => (
+                  <tr key={index}>
+                    <td>{index + 1}</td>
+                    <td>{student.name}</td>
+                    <td>{student.contact}</td>
+                    <td>{student.email}</td>
+                    <td>
+                      <Link to={`edit/${student._id}`}>
+                        <Button variant="warning" className="me-2">
+                          Edit
+                        </Button>
+                      </Link>
+                      <Link to={`/view/${student._id}`}>
+                        <Button variant="info" className="me-2 mt-2 mt-md-0">
+                          View
+                        </Button>
+                      </Link>
+                      <Button
+                        variant="danger"
+                        className="mt-2 mt-md-0"
+                        onClick={() => onDeleteStudent(student._id)}
+                      >
+                        Delete
                       </Button>
-                    </Link>
-                    <Link to={`/view/${student._id}`}>
-                      <Button variant="info" className="me-2 mt-2 mt-md-0">
-                        View
-                      </Button>
-                    </Link>
-                    <Button
-                      variant="danger"
-                      className="mt-2 mt-md-0"
-                      onClick={() => onDeleteStudent(student._id)}
-                    >
-                      Delete
-                    </Button>
-                  </td>
-                </tr>
-              ))}
-          </tbody>
-        </Table>
+                    </td>
+                  </tr>
+                ))}
+            </tbody>
+          </Table>
+        )}
       </Card>
     </Container>
   );
